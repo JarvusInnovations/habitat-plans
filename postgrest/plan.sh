b86534be87a869e3d0ad64975b58cdc5537d53e6
Chris Alfano <chris@jarv.us>
2016-12-31 23:15:25 +0000

    Upgrade plan for new stack package

--- postgrest/plan.sh
+++ postgrest/plan.sh
@@ -2,30 +2,16 @@ pkg_name=postgrest
 pkg_origin=jarvus
 pkg_version=0.x.x
 pkg_source=https://github.com/begriffs/postgrest.git
-ghc_version=8.0.1
+
 pkg_build_deps=(
-  core/git
   jarvus/haskell-stack
-  jarvus/ghc-archive/${ghc_version}
+  core/git
   core/patchelf
+  core/gcc
+  core/postgresql
 )
 pkg_deps=(
-  core/gcc
-  core/glibc
-  core/gmp
-  core/libffi
-  core/gawk
-  core/perl
-  jarvus/ncurses5-compat-libs
 )
-
-do_prepare() {
-  # stack will extract and run ghc and we won't be able to run patchelf on it
-  if [[ ! -r /lib/ld-linux-x86-64.so.2 ]]; then
-    ln -s "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" /lib/
-    _clean_lib=true
-  fi
-}
 
 do_download() {
   return 0
@@ -40,23 +26,12 @@ do_unpack() {
 }
 
 do_build() {
-  export LD_LIBRARY_PATH="$LD_RUN_PATH"
-  export STACK_YAML="$(pkg_path_for ghc-archive)/stack.yaml"
-  export AWK="$(pkg_path_for gawk)/bin/awk"
-
   attach
-  hab pkg exec jarvus/haskell-stack stack build --install-ghc && return 0
+  stack build && return 0
   attach
   return 1
 }
 
 do_install() {
   return 0
-}
-
-do_end() {
-  # Clean up the `lib` link, if we set it up.
-  if [[ -n "$_clean_lib" ]]; then
-    rm -fv /lib/ld-linux-x86-64.so.2
-  fi
 }
