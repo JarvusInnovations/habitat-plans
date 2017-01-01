76dc095d3501ceb0e648a0fb454e77d6b311e90c
Chris Alfano <chris@jarv.us>
2017-01-01 01:04:15 +0000

    Finish build and install binary

--- postgrest/plan.sh
+++ postgrest/plan.sh
@@ -2,16 +2,17 @@ pkg_name=postgrest
 pkg_origin=jarvus
 pkg_version=0.x.x
 pkg_source=https://github.com/begriffs/postgrest.git
+pkg_bin_dirs=(bin)
 
 pkg_build_deps=(
   jarvus/haskell-stack
   core/git
   core/patchelf
   core/gcc
-  core/postgresql
   core/zlib
 )
 pkg_deps=(
+  core/postgresql
 )
 
 do_download() {
@@ -30,11 +31,19 @@ do_build() {
   export LIBRARY_PATH="$LIBRARY_PATH:$(pkg_path_for postgresql)/lib"
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pkg_path_for postgresql)/lib"
 
-  stack build --extra-include-dirs="$(pkg_path_for zlib)/include" && return 0
+  mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname/bin"
+
+  stack build \
+    --extra-include-dirs="$(pkg_path_for zlib)/include" \
+    --copy-bins \
+    --local-bin-path="$HAB_CACHE_SRC_PATH/$pkg_dirname/bin" \
+    && return 0
 
   return 1
 }
 
 do_install() {
+  cp -vr ./bin "$pkg_prefix"
+
   return 0
 }
