e7a96541e5a33ccb410ceb1da8e9a3934b16c944
Chris Alfano <chris@jarv.us>
2017-01-02 20:12:52 +0000

    Enable fpm binary and pass library paths

--- php5/plan.sh
+++ php5/plan.sh
@@ -10,30 +10,43 @@ pkg_source=https://php.net/get/${pkg_distname}-${pkg_version}.tar.bz2/from/this/
 pkg_filename=${pkg_distname}-${pkg_version}.tar.bz2
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_shasum=facd280896d277e6f7084b60839e693d4db68318bfc92085d3dc0251fd3558c7
-pkg_deps=(core/libxml2)
-pkg_build_deps=(core/bison2 core/gcc core/make core/re2c)
-pkg_bin_dirs=(bin)
+pkg_deps=(
+  core/curl
+  core/openssl
+  core/zlib
+  core/libxml2
+  core/glibc
+)
+pkg_build_deps=(
+  core/bison2
+  core/gcc
+  core/make
+  core/re2c
+)
+pkg_bin_dirs=(bin sbin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_interpreters=(bin/php)
 
-do_prepare() {
-  # The configure script expects libxml2 binaries to either be in `/usr/bin`, `/usr/local/bin` or be
-  # passed in as a configure param. Instead of overriding the entire do_build, symlink the
-  # required executable into place.
-  if [[ ! -r /usr/bin/xml2-config ]]; then
-    ln -sv "$(pkg_path_for libxml2)/bin/xml2-config" /usr/bin/xml2-config
-    _clean_xml2=true
-  fi
+do_build() {
+  ./configure --prefix="$pkg_prefix" \
+    --enable-exif \
+    --enable-fpm \
+    --enable-mbstring \
+    --enable-opcache \
+    --with-curl="$(pkg_path_for curl)" \
+    --with-openssl="$(pkg_path_for openssl)" \
+    --with-xmlrpc \
+    --with-zlib="$(pkg_path_for zlib)" \
+    --with-libxml-dir="$(pkg_path_for libxml2)"
+
+  make
+
+  return $?
 }
 
 do_check() {
   make test
-}
 
-do_end() {
-  # Clean up the `xml2-config` link, if we set it up.
-  if [[ -n "$_clean_xml2" ]]; then
-    rm -fv /usr/bin/xml2-config
-  fi
+  return $?
 }
