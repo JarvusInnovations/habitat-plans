911fca8b88b208a7b92c7a012bd466acbb48e056
Chris Alfano <chris@jarv.us>
2017-01-03 23:52:49 +0000

    Update openresty plan and add options from cloudinit

--- openresty/plan.sh
+++ openresty/plan.sh
@@ -1,40 +1,61 @@
 origin=jarvus
 pkg_name=openresty
-pkg_version=1.9.15.1
+pkg_version=1.11.2.2
 pkg_description="OpenResty is a full-fledged web application server by bundling the standard nginx core, lots of 3rd-party nginx modules, as well as most of their external dependencies."
-pkg_maintainer="Yichun Zhang <agentzh@gmail.com>"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
 pkg_license=('BSD-2-Clause')
 pkg_upstream_url=https://openresty.org
 pkg_source=https://openresty.org/download/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=75cf020144048c9013ee487cb48107a5b99de04a5a8fa83839c8b4c3aa4eb0db
+pkg_shasum=7f9ca62cfa1e4aedf29df9169aed0395fd1b90de254139996e554367db4d5a01
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
-pkg_deps=(core/glibc core/libedit core/ncurses core/zlib core/bzip2 core/openssl core/pcre)
-pkg_build_deps=(core/gcc core/make core/coreutils core/perl)
+pkg_deps=(
+  core/glibc
+  core/perl
+  #core/libedit
+  #core/ncurses
+  #core/bzip2
+)
+pkg_build_deps=(
+  core/gcc
+  core/make
+  core/coreutils
+
+  # libraries that MAY be needed at runtime too, check LDD:
+  core/zlib
+  core/openssl
+  core/pcre
+  core/postgresql
+  core/geoip
+)
 pkg_bin_dirs=(bin nginx/sbin luajit/bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_expose=(80 443)
 
 do_build() {
-  ./configure --prefix=${pkg_prefix} \
-              --with-pcre-jit \
-              --with-ipv6 \
-              --with-http_dav_module \
-              --with-http_stub_status_module \
-              --with-http_v2_module \
-              --with-http_gunzip_module \
-              --with-http_gzip_static_module \
-              --with-http_realip_module \
-              --with-http_ssl_module \
-              --with-http_stub_status_module \
-              --with-cc-opt="$CFLAGS" \
-              --with-ld-opt="$LDFLAGS" \
-              -j8
-    make  -j8
-}
+  ./configure \
+    --prefix="${pkg_prefix}" \
+    --with-ld-opt="${LDFLAGS}" \
+    --with-cc-opt="${CFLAGS}" \
+    --with-libpq="$(pkg_path_for postgresql)" \
+    --with-pcre-jit \
+    --with-luajit \
+    --with-http_v2_module \
+    --with-http_dav_module \
+    --with-http_flv_module \
+    --with-http_geoip_module \
+    --with-http_gzip_static_module \
+    --with-http_realip_module \
+    --with-http_stub_status_module \
+    --with-http_ssl_module \
+    --with-http_sub_module \
+    --with-ipv6 \
+    --with-http_stub_status_module \
+    --with-http_secure_link_module \
+    --with-http_sub_module \
+    --with-http_postgres_module
 
-do_install() {
-  make install
+    make -j
 }
 
 # Workaround from: https://github.com/habitat-sh/habitat/issues/1041)
