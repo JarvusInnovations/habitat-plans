7db4bb36378d9ad78e49d157025b7035436eaef2
Chris Alfano <chris@jarv.us>
2017-01-04 07:38:20 +0000

    Setup GOPATH correctly to avoid downloading packages or linking

--- gnatsd/plan.sh
+++ gnatsd/plan.sh
@@ -12,27 +12,32 @@ pkg_build_deps=(core/go core/coreutils core/gcc core/make)
 pkg_bin_dirs=(bin)
 pkg_svc_run="${pkg_name}"
 
+
+do_begin() {
+  export GOPATH="/hab/cache"
+  parent_go_path="${GOPATH}/src/github.com/nats-io"
+}
+
+do_clean() {
+  do_default_clean
+  rm -rf "${parent_go_path}"
+  return $?
+}
+
+do_prepare() {
+  mkdir -p "${parent_go_path}"
+  ln -s "${PWD}" "${parent_go_path}/${pkg_name}"
+  return $?
+}
+
 do_build() {
-  export GOPATH="${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
-
-  local libs_src_path="${GOPATH}/src/github.com/nats-io/gnatsd"
-
-  mkdir -p "${libs_src_path}"
-
-  ln -s \
-    "${GOPATH}/auth" \
-    "${GOPATH}/server" \
-    "${GOPATH}/logger" \
-    "${GOPATH}/conf" \
-    "${GOPATH}/util" \
-    "${GOPATH}/vendor" \
-    "${libs_src_path}"
-
   go build
+  return $?
 }
 
 
 do_install() {
   mkdir -p "${pkg_prefix}/bin"
   cp  "${pkg_name}-${pkg_version}" "${pkg_prefix}/bin/${pkg_name}"
+  return $?
 }
