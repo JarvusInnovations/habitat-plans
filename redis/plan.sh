b433f721904e04b730475b2a816ea3f2d64b2776
Chris Alfano <chris@jarv.us>
2017-01-04 00:31:53 +0000

    Upgrade redis to 4.0-rc2

--- redis/plan.sh
+++ redis/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=redis
 pkg_origin=core
-pkg_version=3.2.4
+pkg_version=4.0-rc2
 pkg_description="Persistent key-value database, with built-in net interface"
 pkg_upstream_url=http://redis.io/
 pkg_license=('BSD-3-Clause')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://download.redis.io/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=2ad042c5a6c508223adeb9c91c6b1ae091394b4026f73997281e28914c9369f1
+pkg_source=https://github.com/antirez/redis/archive/${pkg_version}.tar.gz
+pkg_shasum=70941c192e6afe441cf2c8d659c39ab955e476030c492179a91dcf3f02f5db67
 pkg_bin_dirs=(bin)
 pkg_build_deps=(core/make core/gcc)
 pkg_deps=(core/glibc)
