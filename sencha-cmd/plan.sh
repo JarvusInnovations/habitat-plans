19ab68d883ca7f1d9517945c62a5c098254a41cf
Chris Alfano <chris@jarv.us>
2017-06-06 15:52:04 -0400

    Adjust plan for Sencha CMD 5.1.3.61

--- sencha-cmd/plan.sh
+++ sencha-cmd/plan.sh
@@ -1,8 +1,8 @@
 pkg_name=sencha-cmd
 pkg_origin=jarvus
-pkg_version=6.2.2
-pkg_source=http://cdn.sencha.com/cmd/${pkg_version}/no-jre/SenchaCmd-${pkg_version}-linux-amd64.sh.zip
-pkg_shasum=4884bd60660b8a501487735df40148cbb5deb204ecda62b268206f9b0aef691e
+pkg_version=5.1.3.61
+pkg_source=http://cdn.sencha.com/cmd/${pkg_version}/SenchaCmd-${pkg_version}-linux-x64.run.zip
+pkg_shasum=171ae9f69bde80f663778b68a59a72bb08c72758e7f366556c5f53adab18a6df
 pkg_bin_dirs=(bin)
 pkg_build_deps=(
   core/zip
@@ -38,15 +38,29 @@ do_build() {
 }
 
 do_install() {
-  mkdir "$pkg_prefix/dist"
-  ./SenchaCmd-*.sh -q -a -dir "$pkg_prefix/dist"
-  ln -s ../dist/sencha "$pkg_prefix/bin/"
+  ln -sf "`hab pkg path core/glibc`/lib/ld-linux-x86-64.so.2" /lib64/
+
+  chmod +x ./SenchaCmd-*.run
+  ./SenchaCmd-*.run --mode unattended --prefix "$pkg_prefix"
+  mv "$pkg_prefix/Sencha/Cmd"/5.* "$pkg_prefix/dist"
+  rm -R "$pkg_prefix/Sencha"
+  find "$pkg_prefix/dist/bin" -type f -exec chmod +x {} \;
 
   build_line "Patching ELF binaries:"
   find "$pkg_prefix/dist" -type f -executable \
     -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
     -print \
     -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "${LD_RUN_PATH}" {} \;
+
+  build_line "Generating wrapper script"
+  cat > "$pkg_prefix/bin/sencha" <<- EOM
+#!/bin/sh
+
+exec $pkg_prefix/dist/sencha \$@
+EOM
+
+    chmod +x "$pkg_prefix/bin/sencha"
+
 
   build_line "Creating repo/.sencha directory"
   mkdir -p "${pkg_prefix}/repo"
