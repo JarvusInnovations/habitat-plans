fcac3f96bcdab1110e33e76bc37d81f4d0110dc1
Chris Alfano <chris@jarv.us>
2017-07-25 21:28:15 -0400

    Bump geos to 3.6.2

--- geos/plan.sh
+++ geos/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=geos
 pkg_origin=core
-pkg_version=3.6.1
+pkg_version=3.6.2
 pkg_description="GEOS (Geometry Engine - Open Source) is a C++ port of the ​Java Topology Suite (JTS)."
 pkg_upstream_url=http://trac.osgeo.org/geos
 pkg_license=('LGPL')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=http://download.osgeo.org/geos/geos-${pkg_version}.tar.bz2
-pkg_shasum=4a2e4e3a7a09a7cfda3211d0f4a235d9fd3176ddf64bd8db14b4ead266189fc5
+pkg_shasum=045a13df84d605a866602f6020fc6cbf8bf4c42fb50de237a08926e1d7d7652a
 pkg_build_deps=(
   core/gcc
   core/make
