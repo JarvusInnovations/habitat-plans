80a1e9e9f8e869c17ec77555228176aa3572b1fb
Chris Alfano <chris@jarv.us>
2019-10-03 06:07:33 +0000

    [kubectl] bump version to 1.16.1

--- kubectl/plan.sh
+++ kubectl/plan.sh
@@ -4,9 +4,9 @@ pkg_description="kubectl CLI tool"
 pkg_upstream_url=https://github.com/kubernetes/kubernetes
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_version=1.14.3
+pkg_version=1.16.1
 pkg_source=https://github.com/kubernetes/kubernetes/archive/v${pkg_version}.tar.gz
-pkg_shasum=3896f5beb1f381cd022ca3dcac2ff4ec4660f7471a1f50cca91a4916ddf54eaa
+pkg_shasum=946d53bbbdb78a4525d5e7d0abb50032803b1ebd5718c11d7e29c21e57f61642
 pkg_dirname="kubernetes-${pkg_version}"
 
 pkg_bin_dirs=(bin)
