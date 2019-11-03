b8e69447e6552e1cb51581a01e2dfb2cabacd676
Chris Alfano <chris@jarv.us>
2019-11-03 21:10:12 +0000

    [imagemagick] format deps

--- imagemagick/plan.sh
+++ imagemagick/plan.sh
@@ -7,8 +7,24 @@ pkg_license=('Apache2')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=http://www.imagemagick.org/download/releases/ImageMagick-${pkg_version}.tar.xz
 pkg_shasum=da2f6fba43d69f20ddb11783f13f77782b0b57783dde9cda39c9e5e733c2013c
-pkg_deps=(core/glibc core/zlib core/libpng core/xz core/gcc-libs)
-pkg_build_deps=(core/zlib core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/glibc core/pkg-config)
+pkg_deps=(
+    core/gcc-libs
+    core/glibc
+    core/libpng
+    core/xz
+    core/zlib
+)
+pkg_build_deps=(
+    core/coreutils
+    core/diffutils
+    core/gcc
+    core/glibc
+    core/make
+    core/patch
+    core/pkg-config
+    core/sed
+    core/zlib
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include/ImageMagick-6)
 pkg_lib_dirs=(lib)
