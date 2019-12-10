b9926431b21bdb37e2fcc0eb113bca06ee1f97c9
Chris Alfano <chris@jarv.us>
2019-12-10 20:19:06 +0000

    [netdata] update to latest from core-plans

--- netdata/plan.sh
+++ netdata/plan.sh
@@ -1,13 +1,12 @@
 pkg_name=netdata
 pkg_origin=jarvus
-pkg_version="1.10.0"
+pkg_version=1.17.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=("GPL-3.0")
+pkg_license=("GPL-3.0-or-later")
 pkg_description="netdata is a system for distributed real-time performance and health monitoring."
-pkg_upstream_url="https://github.com/firehol/netdata"
-pkg_source="https://github.com/firehol/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_shasum="645b1cb60a779132c816d1bad377574884992c214bbb0f7a483649878884dbab"
-
+pkg_upstream_url="https://github.com/netdata/netdata"
+pkg_source="https://github.com/netdata/${pkg_name}/archive/v${pkg_version}.tar.gz"
+pkg_shasum=2dc0d510a31655bab4c269b78e1619fcdce38bf79335966d61d6bbd7bed736c5
 pkg_build_deps=(
   core/autoconf
   core/autogen
@@ -16,17 +15,16 @@ pkg_build_deps=(
   core/gcc
   core/make
 )
-
-
 pkg_deps=(
+  core/bash
+  core/curl
+  core/gawk
   core/glibc
+  core/python
   core/util-linux
   core/zlib
-
-  # for fix_interpreter
   core/coreutils
 )
-
 pkg_bin_dirs=(sbin)
 pkg_exports=(
   [host]=server.address
@@ -35,13 +33,11 @@ pkg_exports=(
 pkg_exposes=(port)
 pkg_svc_run="netdata -D -c ${pkg_svc_config_path}/netdata.conf"
 
-
 do_build() {
   # patch shell script shebang lines to use habitat-provided env
   fix_interpreter "./*.sh" core/coreutils bin/env
 
-  ACLOCAL_PATH="$(pkg_path_for core/pkg-config)/share/aclocal" ./autogen.sh
-
+  ACLOCAL_PATH="$(pkg_path_for core/pkg-config)/share/aclocal" autoreconf -ivf
   ./configure \
     --prefix="${pkg_prefix}" \
     --with-zlib \
@@ -54,5 +50,14 @@ do_build() {
 do_install() {
   do_default_install || return $?
 
-  rm -r "${pkg_prefix}/var"
+  pushd "${pkg_prefix}" > /dev/null
+
+  rm -r "./var"
+
+  build_line "Fixing libexec interpreters"
+  find ./libexec/netdata -type f -executable \
+    -print \
+    -exec bash -c 'sed -e "s#\#\!/usr/bin/env bash#\#\!$1/bin/bash#" --in-place "$2"' _ "$(pkg_path_for bash)" "{}" \;
+
+  popd > /dev/null
 }
