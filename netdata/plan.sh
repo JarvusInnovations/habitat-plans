7c1268b47ca438af4714e7c9f4794ba59ca0a156
Chris Alfano <chris@jarv.us>
2020-04-08 03:46:05 +0000

    [netdata] add go.d.plugin

--- netdata/plan.sh
+++ netdata/plan.sh
@@ -12,6 +12,7 @@ pkg_build_deps=(
   core/autogen
   core/automake
   core/gcc
+  core/go
   core/make
   core/pkg-config
 )
@@ -40,12 +41,41 @@ pkg_exposes=(port)
 pkg_svc_user="root"
 pkg_svc_run="netdata -D -c ${pkg_svc_config_path}/netdata.conf"
 
+plugin_go_version=0.18.0
+plugin_go_source="https://github.com/netdata/go.d.plugin/archive/v${plugin_go_version}.tar.gz"
+plugin_go_filename="go.d.plugin-${plugin_go_version}.tar.gz"
+plugin_go_dirname="go.d.plugin-${plugin_go_version}"
+plugin_go_source_shasum=562018e328cbfa6f1e1427af1e4dbb2ab7bd7415c1c36cdcb3ba80e066cc13ec
 
 do_setup_environment() {
   push_runtime_env PYTHONPATH "${pkg_prefix}/lib/python3.7/site-packages"
 
   set_runtime_env -f NETDATA_PKG_CONFIG_DIR "${pkg_prefix}/etc/netdata"
   set_runtime_env -f NETDATA_PKG_WEB_DIR "${pkg_prefix}/share/netdata/web"
+}
+
+do_before() {
+  plugin_go_cache_path="$HAB_CACHE_SRC_PATH/${plugin_go_dirname}"
+}
+
+do_download() {
+  do_default_download
+  download_file "${plugin_go_source}" "${plugin_go_filename}" "${plugin_go_source_shasum}"
+}
+
+do_verify() {
+  do_default_verify
+  verify_file "${plugin_go_filename}" "${plugin_go_source_shasum}"
+}
+
+do_clean() {
+  do_default_clean
+  rm -rf "${plugin_go_cache_path}"
+}
+
+do_unpack() {
+  do_default_unpack
+  unpack_file "${plugin_go_filename}"
 }
 
 do_prepare() {
@@ -57,6 +87,8 @@ do_build() {
   # patch shell script shebang lines to use habitat-provided env
   fix_interpreter "./*.sh" core/coreutils bin/env
 
+  # see https://github.com/netdata/netdata/blob/master/netdata-installer.sh
+  build_line "Building netdata"
   ACLOCAL_PATH="$(pkg_path_for core/pkg-config)/share/aclocal" autoreconf -ivf
   CFLAGS="${CFLAGS} -O2" ./configure \
     --prefix="${pkg_prefix}" \
@@ -65,6 +97,13 @@ do_build() {
     --with-user="${pkg_svc_user}"
 
   make
+
+  # see https://github.com/netdata/go.d.plugin/blob/master/hack/go-build.sh
+  build_line "Building go.d.plugin"
+  pushd "${plugin_go_cache_path}" > /dev/null
+  go mod download
+  CGO_ENABLED=0 go build -o go.d.plugin -ldflags "-w -s -X main.version=${plugin_go_version}" github.com/netdata/go.d.plugin/cmd/godplugin
+  popd > /dev/null
 }
 
 do_install() {
@@ -83,6 +122,10 @@ do_install() {
   build_line "Installing python dependencies"
   pip install "mysqlclient"
   pip freeze > requirements.txt
+
+  build_line "Installing go.d.plugin"
+  cp -v "${plugin_go_cache_path}/go.d.plugin" "./libexec/netdata/plugins.d/"
+  cp -rv "${plugin_go_cache_path}/config"/* "./lib/netdata/conf.d/"
 
   popd > /dev/null
 }
