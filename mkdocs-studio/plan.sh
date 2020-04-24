0791d258d420a86cf040122324ee53ef47bf43bd
Chris Alfano <chris@jarv.us>
2020-04-24 19:08:25 +0000

    feat(mkdocs-studio): add plan

---
+++ mkdocs-studio/plan.sh
@@ -0,0 +1,24 @@
+pkg_name=mkdocs-studio
+pkg_origin=jarvus
+pkg_version=1.0.0
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=('MIT')
+pkg_description="Studio tools for MkDocs"
+pkg_build_deps=(
+)
+pkg_deps=(
+  jarvus/mkdocs
+  jarvus/stoml
+)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp -v "studio.sh" "${pkg_prefix}/"
+}
+
+do_strip() {
+  return 0
+}
---
+++ mkdocs-studio/studio.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+# welcome message and environment detection
+echo
+echo "--> Welcome to MkDocs Studio! Detecting environment..."
+if [ -z "${DOCS_REPO}" ]; then
+    DOCS_REPO="$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd)"
+    DOCS_REPO="${DOCS_REPO:-/src}"
+fi
+echo "    DOCS_REPO=${DOCS_REPO}"
+echo
+
+
+# set up developer commands
+echo
+echo "--> Setting up MkDocs Studio commands..."
+
+echo "    * Use 'watch-docs' to start live-reloading docs server"
+watch-docs() {
+    local requirements="${DOCS_REQUIREMENTS}"
+
+    if [ -z "${requirements}" ]; then
+        echo "\$DOCS_REQUIREMENTS not defined in environment"
+        local lens_config_path="${DOCS_REPO}/.holo/branches/${DOCS_HOLOBRANCH:-gh-pages}.lenses/mkdocs.toml"
+
+        if [ -f "${lens_config_path}" ]; then
+            echo "Loading requirements from ${lens_config_path}"
+            requirements="$(hab pkg exec jarvus/stoml stoml "${lens_config_path}" hololens.requirements)"
+        else
+            echo "Also did not find ${lens_config_path}"
+        fi
+    fi
+
+    echo
+
+    hab pkg exec jarvus/mkdocs python -m venv "/hab/cache/docs.venv"
+
+    (
+        set -e # exit subshell if any commands fail
+        set -h # turn on a shell feature so activate can turn it back off without tripping -e
+        source "/hab/cache/docs.venv/bin/activate";
+
+        pip install --upgrade pip;
+        pip install mkdocs ${requirements};
+
+        mkdocs serve --dev-addr 0.0.0.0:8000;
+    )
+}
+
+echo "    * Use 'bg-watch-docs' to run watch-docs in a background job"
+bg-watch-docs() {
+    watch-docs > /hab/cache/mkdocs.log 2>&1 &
+
+    jobs -l %%
+    echo
+    echo "* job launched to background, use 'kill %<job-id>' to terminate"
+    echo
+}
