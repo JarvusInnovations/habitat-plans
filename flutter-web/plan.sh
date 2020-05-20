a0680473bdc6ec0be6a01aaa13d936b667108954
Chris Alfano <chris@jarv.us>
2020-05-20 02:38:33 +0000

    refactor(flutter-web): reimplement like nixos does

--- flutter-web/plan.sh
+++ flutter-web/plan.sh
@@ -1,11 +1,22 @@
+# References:
+# - https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/compilers/flutter
+# - https://github.com/NixOS/nixpkgs/issues/36759
+
 pkg_name=flutter-web
 pkg_origin=jarvus
-pkg_version="1.18.0-11.1.pre"
-pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_upstream_url="https://flutter.dev"
 pkg_license=("BSD 3-Clause")
+pkg_version="1.18.0-11.1.pre"
+pkg_channel="beta"
+pkg_filename="flutter_linux_${pkg_version}-${pkg_channel}.tar.xz" # TODO: drop v prefix beyond 1.15.x
+pkg_dirname="flutter"
+pkg_source="https://storage.googleapis.com/flutter_infra/releases/${pkg_channel}/linux/${pkg_filename}"
+pkg_shasum="ec4535ec5bdb0f3d7549a81a31a8975febe998efafae44806cdb37173513cc55"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
 
 pkg_deps=(
   core/bash
+  core/cacerts
   core/coreutils
   core/curl
   core/git
@@ -14,110 +25,85 @@ pkg_deps=(
   core/unzip
   core/which
   core/xz
-  core/zip
 )
 
 pkg_build_deps=(
+  core/patch
   core/patchelf
 )
 
 pkg_bin_dirs=(bin)
 
-cache_dart_version="2.9.0-8.2.beta"
-cache_dart_source="https://storage.googleapis.com/dart-archive/channels/beta/release/${cache_dart_version}/sdk/dartsdk-linux-x64-release.zip"
-cache_dart_filename="dartsdk-${cache_dart_version}.zip"
-cache_dart_shasum=ac2a85c4fd4918b580175d259464dd82b7153236e202265d39cba8d9ef845ba1
 
+do_setup_environment() {
+  set_buildtime_env FLUTTER_ROOT "${CACHE_PATH}"
+  set_buildtime_env FLUTTER_TOOLS_DIR "${CACHE_PATH}/packages/flutter_tools"
+  set_buildtime_env SCRIPT_PATH "${CACHE_PATH}/packages/flutter_tools/bin/flutter_tools.dart"
+  set_buildtime_env SNAPSHOT_PATH "${CACHE_PATH}/bin/cache/flutter_tools.snapshot"
+  set_buildtime_env STAMP_PATH "${CACHE_PATH}/bin/cache/flutter_tools.stamp"
+  set_buildtime_env DART_SDK_PATH "${CACHE_PATH}/bin/cache/dart-sdk"
+  set_buildtime_env PUB_CACHE "${CACHE_PATH}/.pub-cache"
 
-do_begin() {
-  if [[ ! -e "/usr/bin/env" ]]; then
-    mkdir -p /usr/bin
-    hab pkg binlink core/coreutils env -d /usr/bin
-    _env_binlink=true
-  fi
+  set_buildtime_env DART "${CACHE_PATH}/bin/cache/dart-sdk/bin/dart"
+  set_buildtime_env PUB "${CACHE_PATH}/bin/cache/dart-sdk/bin/pub"
 
-  if [[ ! -e "/lib64/ld-linux-x86-64.so.2" ]]; then
-    mkdir -p /lib64
-    ln -sv "$(hab pkg path core/glibc)/lib/ld-linux-x86-64.so.2" "/lib64/ld-linux-x86-64.so.2"
-    _ld_solink=true
-  fi
-}
-
-do_download() {
-    download_file "${cache_dart_source}" "${cache_dart_filename}" "${cache_dart_shasum}"
-}
-
-do_verify() {
-  return 0
-}
-
-do_unpack() {
-  return 0
+  set_runtime_env DART_VM_OPTIONS "--root-certs-file=$(pkg_path_for cacerts)/ssl/certs/cacert.pem"
+  set_runtime_env FLUTTER_WEB "true"
 }
 
 do_build() {
-  return 0
-}
+  build_line "Applying patches"
+  for patch in "${PLAN_CONTEXT}/patches/"*.patch; do
+    patch -p1 < "${patch}"
+  done
 
-do_install() {
-  pushd "${pkg_prefix}" > /dev/null
-
-  build_line "Cloning Flutter SDK"
-  rm -r bin
-  git clone --branch "${pkg_version}" --depth 1 "https://github.com/flutter/flutter" .
-
-  build_line "Caching Dart SDK"
-  mkdir bin/cache
-  unzip "${HAB_CACHE_SRC_PATH}/${cache_dart_filename}" -d bin/cache
-  find bin/cache/dart-sdk -type d -exec chmod 755 {} \;
-  find bin/cache/dart-sdk -type f -perm /u+x -exec chmod a+x,a+r {} \;
-  cp bin/internal/engine.version bin/cache/engine-dart-sdk.stamp
-
-  build_line "Fixing bash interpreters"
-  grep -nrlI '^\#\!/usr/bin/env' . | while read -r target; do
+  build_line "Fixing shebang lines"
+  grep -nrlI '^\#\!/usr/bin/env' ./bin/ | while read -r target; do
     echo "${target}"
     sed -e "s#\#\!/usr/bin/env sh#\#\!$(pkg_path_for bash)/bin/sh#" -i "${target}"
     sed -e "s#\#\!/usr/bin/env bash#\#\!$(pkg_path_for bash)/bin/bash#" -i "${target}"
   done
 
-  # build_line "Fixing ELF interpreters"
-  # find . -type f -executable \
-  #     -exec sh -c 'file -i "$1" | grep -q "x-\(pie-\)\?executable; charset=binary"' _ {} \; \
-  #     -print \
-  #     -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" {} \;
+  build_line "Fixing ELF interpreters"
+  find . -type f -executable \
+    -exec sh -c 'file -i "$1" | grep -q "x-\(pie-\)\?executable; charset=binary"' _ {} \; \
+    -print \
+    -exec patchelf --set-interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" {} \;
 
-  build_line "Running flutter precache"
-  flutter precache \
-    --verbose \
-    --web \
-    --no-android \
-    --no-ios \
-    --no-linux \
-    --no-windows \
-    --no-macos \
-    --no-fuchsia \
-    --no-universal
-
-  build_line "Preparing for web builds"
-  flutter config --enable-web
-
-  pushd "${HAB_CACHE_SRC_PATH}" > /dev/null
-  flutter create "flutter_web_app" # easiest way to pre-populate cache with web artifacts
-  rm -rf "./flutter_web_app"
+  build_line "Running: pub upgrade"
+  pushd "${FLUTTER_TOOLS_DIR}" > /dev/null
+  "${PUB}" upgrade --offline
   popd > /dev/null
 
-  build_line "Making version and cache directory openly writable"
-  chmod -R go+w version bin/cache/
+  build_line "Updating stamp and version"
+  local revision="$(git rev-parse HEAD)"
+  "${DART}" --snapshot="${SNAPSHOT_PATH}" --packages="${FLUTTER_TOOLS_DIR}/.packages" "${SCRIPT_PATH}"
+  echo "${revision}" > "${STAMP_PATH}"
+  echo -n "${pkg_version}" > version
 
+  build_line "Cleaning up caches"
+  rm -rf bin/cache/{artifacts,downloads}
+  rm -f  bin/cache/*.stamp
+  rm -rf .pub-cache
+
+  return 0
+}
+
+do_install() {
+  cp -r . "${pkg_prefix}"
+
+  build_line "Wrapping flutter command"
+  pushd "${pkg_prefix}" > /dev/null
+  mv "bin/flutter" "bin/flutter.real"
+  cat <<END_OF_WRAPPER > "bin/flutter"
+#!$(pkg_path_for bash)/bin/bash
+export PUB_CACHE=\${PUB_CACHE:-"\$HOME/.pub-cache"}
+exec ${pkg_prefix}/bin/flutter.real --no-version-check "\$@"
+END_OF_WRAPPER
+  chmod +x "bin/flutter"
   popd > /dev/null
 }
 
-do_end() {
-  if [[ -v $_env_binlink ]]; then
-    rm -f "/usr/bin/env"
-  fi
-
-  if [[ -v $_ld_solink ]]; then
-    rm -f "/lib64/ld-linux-x86-64.so.2"
-  fi
+do_strip() {
+  return 0
 }
