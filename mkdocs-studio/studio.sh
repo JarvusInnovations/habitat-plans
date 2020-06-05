11e330bf51184df459fa6d531857363336841432
Chris Alfano <chris@jarv.us>
2020-06-05 05:41:30 +0000

    feat: add docs-stop and docs-log commands

--- mkdocs-studio/studio.sh
+++ mkdocs-studio/studio.sh
@@ -17,7 +17,7 @@ declare -A STUDIO_HELP
 echo
 echo "--> Setting up MkDocs Studio commands..."
 
-STUDIO_HELP[docs-watch]="start live-reloading docs server"
+STUDIO_HELP[docs-watch]="Start live-reloading docs server"
 docs-watch() {
     local requirements="${DOCS_REQUIREMENTS}"
 
@@ -40,6 +40,8 @@ docs-watch() {
     (
         set -e # exit subshell if any commands fail
         set -h # turn on a shell feature so activate can turn it back off without tripping -e
+
+        cd "${DOCS_REPO}"
         source "/hab/cache/docs.venv/bin/activate";
 
         pip install --upgrade pip;
@@ -49,18 +51,38 @@ docs-watch() {
     )
 }
 
-STUDIO_HELP[docs-start]="start live-reloading docs server in background job"
+STUDIO_HELP[docs-start]="Start live-reloading docs server in background job"
 docs-start() {
+    docs-stop || true
+
     echo
     echo "--> Launching \`mkdocs serve\` in the background..."
 
-    docs-watch > /hab/cache/mkdocs.log 2>&1 &
+    docs-watch > ~/mkdocs.log 2>&1 &
     _docs_pid=$!
 
-    jobs -l %%
+    echo -n "    Waiting for server"
+    until curl --fail "localhost:${DOCS_PORT:-8000}/index.html" 2>/dev/null >/dev/null; do echo -n "."; sleep .5; done;
     echo
-    echo "    * job launched to background, use 'kill %<job-id>' or 'kill ${_docs_pid} to terminate"
+
     echo
+    echo -e "    \e[1m* MkDocs hot-reload now available: \e[92mhttp://localhost:${DOCS_PORT:-8000}\e[0m"
+}
+
+STUDIO_HELP[docs-stop]="Stop live-reloading docs server started in background"
+docs-stop() {
+    [ -n "${_docs_pid}" ] && {
+        echo
+        echo "--> Stopping docs server..."
+        echo "    Killing process #${_docs_pid}"
+        kill -- "-${_docs_pid}" # kill entire group
+        unset _docs_pid
+    }
+}
+
+STUDIO_HELP[docs-log]="Open and follow mkdocs serve output"
+docs-log() {
+    less -R +F ~/mkdocs.log
 }
 
 echo
