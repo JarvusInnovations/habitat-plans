e796e5e3ec9d45ba64177e3b55ba574fbe322106
Chris Alfano <chris@jarv.us>
2020-06-05 06:39:27 +0000

    feat(studio-toolkit): add studio-svc-config command

--- studio-toolkit/studio.sh
+++ studio-toolkit/studio.sh
@@ -20,4 +20,27 @@ studio-help() {
     echo
 }
 
+STUDIO_HELP[studio-svc-config]="Write \$2 configuration to /hab/user/\$1/config/user.toml"
+studio-svc-config() {
+    local config_force
+    if [ "$1" == "--force" ]; then
+        shift
+        config_force=true
+    else
+        config_force=false
+    fi
+
+    local config_pkg_name="$1"
+    local config_default="$2"
+    [ -z "$config_pkg_name" -o -z "$config_default" ] && { echo >&2 'Usage: init-user-config pkg_name "[default]\nconfig = value"'; return 1; }
+
+    local config_toml_path="/hab/user/${config_pkg_name}/config/user.toml"
+
+    if $config_force || [ ! -f "$config_toml_path" ]; then
+        echo "    Initializing: $config_toml_path"
+        mkdir -p "/hab/user/${config_pkg_name}/config"
+        echo -e "$config_default" | awk '{$1=$1};1NF' | awk 'NF' > "$config_toml_path"
+    fi
+}
+
 echo
