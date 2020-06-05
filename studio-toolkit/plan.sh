f68a6d562cd1d4592627cab684ce5b788f226614
Chris Alfano <chris@jarv.us>
2020-06-05 03:35:48 +0000

    feat(studio-toolkit): add plan

---
+++ studio-toolkit/plan.sh
@@ -0,0 +1,23 @@
+pkg_name=studio-toolkit
+pkg_origin=jarvus
+pkg_version=1.0.0
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=('MIT')
+pkg_description="Toolkit for providing development environments via Chef Habitat Studios"
+pkg_build_deps=(
+)
+pkg_deps=(
+  core/bash
+)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp -v "studio.sh" "${pkg_prefix}/"
+}
+
+do_strip() {
+  return 0
+}
---
+++ studio-toolkit/studio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+declare -A STUDIO_HELP
+
+echo
+echo "--> Setting up studio toolkit..."
+
+STUDIO_HELP[studio-help]="show all registered studio commands"
+studio-help() {
+    echo
+    echo "--> Available studio commands:"
+    echo
+
+    local _commandsWidth=$(printf '%s\n' "${!STUDIO_HELP[@]}" | wc -L)
+
+    for command in $(printf '%s\n' "${!STUDIO_HELP[@]}" | sort); do
+        printf "    \e[92m\e[1m%-${_commandsWidth}.${_commandsWidth}s\e[0m  %s\n" "${command}" "${STUDIO_HELP[$command]}"
+    done
+
+    echo
+}
+
+echo
