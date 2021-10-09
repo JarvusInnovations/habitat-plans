dd9230c68be350577cf382a874791331d026ed01
Chris Alfano <chris@jarv.us>
2021-10-09 10:19:48 -0400

    chore(rclone): bump version to 1.56.2

--- rclone/plan.sh
+++ rclone/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=rclone
 pkg_origin=jarvus
-pkg_version="1.49.4"
+pkg_version="1.56.2"
 pkg_maintainer="Chris Alfano <chris@jarv.us>"
 pkg_plan_source="https://github.com/JarvusInnovations/habitat-plans/tree/master/rclone"
 pkg_license=("MIT")
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_shasum="d21baf46551d9f3ee33dbc921cfaac503b0142339bdcc75eee973f7ed4b7a354"
+pkg_shasum="a5b0b7dfe17d9ec74e3a33415eec4331c61d800d8823621e61c6164e8f88c567"
 
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/go)
