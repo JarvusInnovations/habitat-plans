f1d6f59aa3425c37d431b62082ba811bdfcbe899
Chris Alfano <chris@jarv.us>
2022-08-03 17:12:31 -0400

    chore(imagemagick): bump version to v7.1.0-45

--- imagemagick/plan.sh
+++ imagemagick/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=imagemagick
 pkg_origin=jarvus
-pkg_version=7.1.0-35
+pkg_version=7.1.0-45
 pkg_description="A software suite to create, edit, compose, or convert bitmap images."
 pkg_upstream_url="http://imagemagick.org/"
 pkg_license=('Apache2')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://www.imagemagick.org/download/releases/ImageMagick-${pkg_version}.tar.xz
-pkg_shasum=26063653de155bab820894e5216a38577420c4dfd59e784daca6601e37f294e0
+pkg_source=https://github.com/ImageMagick/ImageMagick/archive/${pkg_version}.tar.gz
+pkg_shasum=3df6ca6dff15a4e8a20b4593c60285a59e38890440494d91a344e5c0e2bb3eec
 pkg_deps=(
     core/gcc-libs
     core/glibc
